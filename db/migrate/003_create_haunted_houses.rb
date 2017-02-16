885d7f0dc3ef266b4c663372c6d9084366bf7fb8
“aroncey” <artuhrroncey@gmail.comssh-keygen -t rsa -C arthurroncey@gmail.com>
2017-02-16 14:53:51 -0500

    Done.

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,16 @@
-# Create your haunted_houses migration here
+# Create your haunted_houses migration hereclass CreateCostumesStores < ActiveRecord::Migration
+class CreateHauntedHouses < ActiveRecord::Migration
+
+  def change
+    create_table :haunted_houses do |t|
+      t.string  :name
+      t.string :location
+      t.string :theme
+      t.float :price
+      t.boolean :family_friendly
+      t.datetime :closing_date
+      t.text :description
+      t.datetime :opening_date
+    end
+  end
+end
